a82657b92cf925245e8c97ce64d046ca31ee43dd
EyupCan <eyupcankilincdemir@gmail.com>
2017-07-04 10:39:29 +0300

    basit if örnegi

---
+++ 003 - if.py
@@ -0,0 +1,35 @@
+#####################################
+#                                   #
+#                                   #
+#                                   #
+#      Eyüp Can KILINÇDEMİR         #
+#    eyupcankilincdemir@gmail.com   #
+#    	ceksoft.wordpress.com       #
+#     							    #
+#                                   #
+#                                   #
+#####################################
+
+# Basit Sayı Kontrolü
+sayi = int(input("Bir sayi giriniz: "))
+print("-"*30)
+if(sayi > 24):
+	print("Girdiğiniz sayi 24 ten büyüktür")
+if(sayi == 24):
+	print("Girdiğiniz sayi 24 e eşittir.")
+if(sayi < 24):
+	print("Girdiğiniz sayi 24 ten küçüktür")
+print("-"*30)
+# Basit String Kontrolü
+print("-"*30)
+print("""
+     Wuhuuu Eyüp Can Parola Kontrol Mekanizmasına
+                    HOŞGELDİNİZ
+""")
+print("-"*30)
+parola = input("Parola Giriniz: ")
+if(parola == "fibonacci"):
+	print("Böyle parolamı konur arkadaş!!")
+if(parola != "fibonacci"):
+	print("Yanlış parola")
+print("-"*30)
