d6d235d34eced1dbe97f8a401020da6f8a142482
EyupCan <eyupcankilincdemir@gmail.com>
2017-07-04 10:41:02 +0300

    basit elif örnegi

---
+++ 004 - elif.py
@@ -0,0 +1,25 @@
+#####################################
+#                                   #
+#                                   #
+#                                   #
+#      Eyüp Can KILINÇDEMİR         #
+#    eyupcankilincdemir@gmail.com   #
+#    	ceksoft.wordpress.com       #
+#     							    #
+#                                   #
+#                                   #
+#####################################
+
+# Basit elif örneği
+print("~"*30)
+yas = int(input("Yaşını gir bakalım: "))
+
+if yas == 18:
+	print("18 yaş güzeldir kardeşş")
+elif yas < 0:
+	print("Yahu - yaş mı olur!!")
+elif yas < 18:
+	print("Hayat yeni başlıyor..")
+elif yas > 18:
+	print("Yaşlandık be dostum :)")
+print("~"*30)
